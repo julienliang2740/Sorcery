538e2a6b395990873eddb79cc634e2d0f2e56069
Julien <julienliang2740@gmail.com>
2023-12-04 18:26:09 -0500

    made minioncomponent iterator return

--- minioncomponent.cc
+++ minioncomponent.cc
@@ -17,6 +17,7 @@ MinionComponent::Iterator::Iterator( MinionComponent* cur): cur{cur} {}
 
 MinionComponent::Iterator MinionComponent::Iterator::operator++() {
     cur = cur->next;
+    return MinionComponent::Iterator{next};
 }
 
 bool MinionComponent::Iterator::operator!=(const MinionComponent::Iterator& other) const {
